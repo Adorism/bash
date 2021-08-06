25cf1c8230f452fa733a5dcffcca24aadba6eef0
Audrey Maldonado <audrey.maldonado@gmail.com>
2021-08-06 15:26:53 -0400

    bash and pwd updates) git push origin main

--- bash.js
+++ bash.js
@@ -1,15 +1,4 @@
-// const fs = require("fs");
+const pwd = require("./pwd.js")
 
-process.stdout.write("prompt >");
 
-process.stdin.on("data", (data) => {
-  const cmd = data.toString().trim();
 
-  if (cmd === "pwd") {
-    process.stdout.write(process.cwd());
-  } else {
-    process.stdout.write("You typed: " + cmd);
-  }
-
-  process.stdout.write("\nprompt > ");
-});
